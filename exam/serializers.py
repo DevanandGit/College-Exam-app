56ea1763f24c7808f33270f1163463cd5ff1d2f0
DevanandGit <nandhudevanand4419@gmail.com>
2023-09-12 20:57:43 +0530

    email id problem solved

--- exam/serializers.py
+++ exam/serializers.py
@@ -217,6 +217,7 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(
             first_name = validated_data['first_name'],
             last_name = validated_data['last_name'],
+            email = validated_data['email'],
             username = validated_data['username'],
             password = validated_data['password']
         )
